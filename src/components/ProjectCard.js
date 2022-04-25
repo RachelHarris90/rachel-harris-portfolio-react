b38dfb89d7259b63832019d2ce14d7c259c88ba3
Rachel Harris <rachel.harris@msn.com>
2022-04-25 22:00:21 +1000

    Improve project section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ const ProjectCard = ({ projectName, imageSrc, imageAlt, siteLink, repoLink }) =>
     return (
         <div className="project-card">
           <h3>{projectName}</h3>
-            <img className="project-examples" src={imageSrc} alt={imageAlt}/>
+            <img className="project-examples" src={imageSrc}/>
             <a className="button" href={siteLink}>Site</a>
             <a className="button" href={repoLink}>Repo</a>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,50 +5,51 @@ const Projects = () => {
   const projectData = [
     {
       projectName: 'artJacent',
-      imageSrc: `"./assets/project-examples/artjacent.png"`,
-      siteLink: `https://artjacent.herokuapp.com/`,
-      repoLink: `"https://github.com/jordanlrothwell/artJacent"`
+      imageSrc: './assets/project-examples/artjacent.png',
+      siteLink: 'https://artjacent.herokuapp.com/',
+      repoLink: 'https://github.com/jordanlrothwell/artJacent'
     },
     {
       projectName: 'Cooped up cooking',
-      imageSrc: `https://github.com/oliverlkelly/cooped-up-cooking/raw/main/assets/images/recipe-finder.png`,
-      siteLink: `"https://oliverlkelly.github.io/cooped-up-cooking/"`,
-      repoLink: `"https://github.com/oliverlkelly/cooped-up-cooking"`
+      imageSrc: 'https://github.com/oliverlkelly/cooped-up-cooking/raw/main/assets/images/recipe-finder.png',
+      siteLink: 'https://oliverlkelly.github.io/cooped-up-cooking/',
+      repoLink: 'https://github.com/oliverlkelly/cooped-up-cooking'
     },
     {
       projectName: 'Team profile generator',
-      imageSrc: `"./assets/images/webpage-example.png"`,
-      siteLink: `"https://github.com/RachelHarris90/team-profile-generator"`,
-      repoLink: `"https://github.com/RachelHarris90/team-profile-generator"`
+      imageSrc: 'https://github.com/RachelHarris90/team-profile-generator/raw/master/assets/command-line-example.png',
+      siteLink: 'https://github.com/RachelHarris90/team-profile-generator',
+      repoLink: 'https://github.com/RachelHarris90/team-profile-generator'
     },
     {
       projectName: 'Australian geography quiz',
-      imageSrc: `"./assets/images/coding-quiz.png" alt="Coding quiz site"`,
-      siteLink: `"https://rachelharris90.github.io/coding-quiz/"`,
-      repoLink: `"https://github.com/RachelHarris90/coding-quiz"`
+      imageSrc: 'assets/project-examples/coding-quiz.png',
+      siteLink: 'https://rachelharris90.github.io/coding-quiz/',
+      repoLink: 'https://github.com/RachelHarris90/coding-quiz'
     },
     {
       projectName: 'Employee tracker',
-      imageSrc: `"./assets/images/initial-prompts.png"`,
-      siteLink: `"https://github.com/RachelHarris90/employee-tracker"`,
-      repoLink: `"https://github.com/RachelHarris90/employee-tracker"`
+      imageSrc: './assets/images/initial-prompts.png',
+      siteLink: 'https://github.com/RachelHarris90/employee-tracker',
+      repoLink: 'https://github.com/RachelHarris90/employee-tracker'
     },
     {
       projectName: 'Day scheduler',
-      imageSrc: `"./assets/images/work-day-scheduler.png"`,
-      siteLink: `"https://rachelharris90.github.io/calendar-app/"`,
-      repoLink: `"https://github.com/RachelHarris90/calendar-app"`,
+      imageSrc: './assets/images/work-day-scheduler.png',
+      siteLink: 'https://rachelharris90.github.io/calendar-app/',
+      repoLink: 'https://github.com/RachelHarris90/calendar-app'
     },
     {
       projectName: 'Weather dashboard',
-      imageSrc: `"./assets/images/weather-dashboard.png"`,
-      siteLink: `"https://rachelharris90.github.io/weather-dashboard/`,
-      repoLink: `"https://github.com/RachelHarris90/weather-dashboard"`
+      imageSrc: './assets/images/weather-dashboard.png',
+      siteLink: 'https://rachelharris90.github.io/weather-dashboard/',
+      repoLink: 'https://github.com/RachelHarris90/weather-dashboard'
     },
   ];
 
   return (
-    <div>
+    <div className="project-container">
+      <a name="projects"></a>
       <h1>Projects</h1>
       <div className="projects" >
         {projectData.map(({ projectName, imageSrc, siteLink, repoLink }) => (
